bfa31fe399a66dbebe796fa71b9257a28fb05aed
Andrii Pryimak <Andrii.Pryimak@live.com>
2015-12-09 19:22:31 +0200

    Simple blog creation

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,18 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', 'ArticlesController@index');
+//
+//Route::get('articles/create', 'ArticlesController@create');
+//
+//Route::get('articles/{id}', 'ArticlesController@show');
+//
+//Route::post('articles', 'ArticlesController@store');
+//
+//Route::get('articles/{id}/edit', 'ArticlesController@edit');
+
+Route::resource('articles', 'ArticlesController');
